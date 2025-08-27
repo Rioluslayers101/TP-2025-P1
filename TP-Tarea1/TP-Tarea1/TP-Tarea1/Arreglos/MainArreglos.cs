431001362e3f42e053ae25e3db9295c4ae3adefd
LUAG27 <giovanny.luna.arrieta@gmail.com>
2025-08-27 10:48:35 -0600

    Arreglos

---
+++ TP-Tarea1/TP-Tarea1/TP-Tarea1/Arreglos/MainArreglos.cs
@@ -0,0 +1,72 @@
+﻿//Arreglos
+//Inicialización
+
+int[] numeros = new int[3];
+
+//Asignar valores  a elementos
+numeros[0] = 0;
+numeros[1] = 2;
+numeros[2] = 4;
+
+for (int i=0; i<=2; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
+
+if (numeros[1] == 2)
+{
+    Console.WriteLine("El valor es 2");
+}
+
+//Obtenener la longitud del arreglo
+Console.WriteLine(numeros.Length);
+
+//Definicion implicita
+int[] numeros2 = new int[10] {3, 4, 1, 2, 7, 5, 8, 9, 11,12};
+char [] vocales = new char[] {'a', 'e', 'i', 'o', 'u'};
+
+foreach (char vocal in vocales)
+{ 
+    Console.WriteLine(vocal);
+}
+
+bool[] estados = new bool[3];
+estados[0] = true;
+estados[1] = false;
+estados[2] = true;
+
+if (estados[2])
+{
+    Console.WriteLine("El estado es verdadero");
+}
+
+//Ordenamiento de menor a mayor
+Console.WriteLine("Arreglo desordenado");
+
+foreach (int numero2 in numeros2)
+{
+    Console.WriteLine(numero2);
+}
+
+Array.Sort(numeros2);
+Console.WriteLine("Arreglo ordenado");
+foreach (int numero2 in numeros2)
+{
+    Console.WriteLine(numero2);
+}
+
+//Arrgelo ordenado de mayor a menor
+//Necesairo mi arreglo ya previamente de menor a mayor
+Array.Reverse(numeros2);
+Console.WriteLine("Arreglo ordenado de mayor a menor");
+    foreach (int numero2 in numeros2)
+{
+    Console.WriteLine(numero2);
+}
+//Busqueda por valor
+Console.WriteLine(Array.BinarySearch(numeros2, 7));
---
+++ TP-Tarea1/TP-Tarea1/TP-Tarea1/Ejercicio Suministros/Inventario.cs
@@ -0,0 +1,60 @@
+﻿public class Inventario
+{
+    //Atributo
+    private Suministro[] suministros;
+
+    //Constructor
+    public Inventario()
+    {
+        suministros = new Suministro[0];
+        {
+            new Suministro("Comida", 5, 1);
+            new Suministro("Agua", 10, 1);
+            new Suministro("Ropa", 7, 2);
+            new Suministro("Medicinas", 0, 1);
+            new Suministro("Herrmientas");
+            new Suministro("Oxigeno");
+            new Suministro("Combustible");
+
+        }
+    }
+
+    //Metodos
+    public void MostrarSuministros()
+    {
+        Console.WriteLine("Suministros en el inventario:");
+        foreach (Suministro item in suministros)
+        {
+            item.MostrarInfo();
+        }
+
+    }
+
+
+    public void BuscarSuministro(string nombre)
+    {
+        int indice = Array.FindIndex(suministros, itemB => itemB.Nombre.ToLower() == nombre.ToLower());
+        if (indice >=0)
+        { 
+          Console.WriteLine($"{nombre} encontrado en la posición {indice}");
+        }
+        else
+        {
+            Console.WriteLine($"{nombre} no esta en el inventario");
+        }
+    }
+
+    //Orednar suministro por nombre
+    public void OrdenarPorNombre()
+    {
+        Array.Sort(suministros,(x,y) => x.Nombre.CompareTo(y.Nombre));
+        Console.WriteLine("El inventario ha sido ordenado por nombre");
+    }
+
+    //Invertir el orden
+    public void InvertirOrden()
+    {
+        Array.Reverse(suministros);
+        Console.WriteLine("El orden del inventario ha sido invertido");
+    }
+}
