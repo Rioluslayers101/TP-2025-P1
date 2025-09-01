4af68d94ac5cdd47801a3e2c9790cbdcd50d1953
LUAG27 <giovanny.luna.arrieta@gmail.com>
2025-09-01 10:54:18 -0600

    Banco y Listas

---
+++ TP-Tarea1/TP-Tarea1/TP-Tarea1/Banco Excepciones/MainBanco.cs
@@ -0,0 +1,48 @@
+﻿//Programa principal del Banco
+Banco banco = new Banco();
+
+try 
+{
+    Console.WriteLine("Ingrese el numero de cuenta:");
+    string numeroCuenta = Console.ReadLine()??"";
+
+    CuentaBancaria cuenta = banco.BuscarCuenta(numeroCuenta);
+
+    //Depositar
+
+    Console.WriteLine("Ingrese la cantidad:");
+    double num = double.Parse(Console.ReadLine()??"");
+    cuenta.Depositar((decimal)num);
+
+
+    //Transferencia
+
+    Console.WriteLine("Ingrese el numero de dcuenta:");
+    string numeCuentaDestino = Console.ReadLine() ?? "";
+
+    CuentaBancaria cuentaDestino = banco.BuscarCuenta(numeCuentaDestino);
+    Console.WriteLine("Ingrese la cantidad a transferir:");
+
+    double numTrans = double.Parse(Console.ReadLine() ?? "");
+
+    cuenta.Transferir(cuentaDestino, (decimal)numTrans);
+
+    //Retirar
+    
+    Console.WriteLine("Ingrese la cantidad a retirar:");
+    double numRet = double.Parse(Console.ReadLine() ?? "");
+    cuenta.Retirar((decimal)numRet);
+}
+
+catch (SaldoInsuficienteException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch (CuentaNoEncontradaException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch (DepositoInvalidoException e)
+{
+    Console.WriteLine(e.Message);
+}
---
+++ TP-Tarea1/TP-Tarea1/TP-Tarea1/Lista/Lista.cs
@@ -0,0 +1,25 @@
+﻿//Listas
+
+//Crear una lista
+
+List<int> numeros = new List<int>();
+List<string> nombres = new List<string>();
+
+//Agregar elementos a la lista
+numeros.Add(10);
+numeros.Add(20);
+numeros.Add(30);
+
+//Acceder a elementos de la lista
+int primerNumero = numeros[0]; // 10
+Console.WriteLine($"Primer numero: {primerNumero}");
+
+//Quitar elementos de la lista
+numeros.Remove(20); // Quita el elemento con valor 20
+numeros.RemoveAt(0); // Quita el elemento en el índice 0 (10)
+
+//Recorrer la lista
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
