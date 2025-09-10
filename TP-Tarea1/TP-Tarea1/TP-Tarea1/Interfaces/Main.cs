7e2555b62d2039853d0a2d3416b0f6a642bbcdd3
LUAG27 <giovanny.luna.arrieta@gmail.com>
2025-09-10 10:55:34 -0600

    Juego terminado y interfaces

---
+++ TP-Tarea1/TP-Tarea1/TP-Tarea1/Interfaces/Main.cs
@@ -0,0 +1,33 @@
+﻿//Porgama Principal
+
+IEjemplo elgo = new IEjemplo.Ejemplo();
+elgo.HacerAlgo();
+elgo.HacerAlgoMas();
+
+Ejemplo otroElgo = new IEjemplo.Ejemplo();
+otroElgo.HacerAlgo();
+otroElgo.HacerAlgoMas();
+//Interfaz
+public interface IEjemplo
+{
+    //Metodo
+    void HacerAlgo();
+    //Metodo 
+    void HacerAlgoMas();
+  
+    //Clase que implementa la interfaz
+
+    public class Ejemplo : IEjemplo
+    {
+        //Implementacion del metodo
+        public void HacerAlgo()
+        {
+            Console.WriteLine("Haciendo algo...");
+        }
+        public void HacerAlgoMas()
+        {
+            Console.WriteLine("Haciendo algo mas...");
+        }
+
+    }
+}
--- TP-Tarea1/TP-Tarea1/TP-Tarea1/Juego/MainJuego.cs
+++ TP-Tarea1/TP-Tarea1/TP-Tarea1/Juego/MainJuego.cs
@@ -1,12 +1,76 @@
 ﻿//Programa pincipal del juego
 try
-{ 
+{
     Console.WriteLine("Bienvenido al juego de guerreros!");
+    //Obtener nombre del jugador
+    string nombreJugardor = ObtenerNombre();
+    //Seleccionar clase del guerrero
+    Guerrero jugador = SeleccionarClase(nombreJugardor);
+    Guerrero enemigo = GenerarEnemigo();
+
+    Console.WriteLine($"Te enfrentas a un {enemigo.Nombre} con {enemigo.Vida} de vida y {enemigo.Ataque} de ataque!");
+    //Bucle principal del juego
+    while (jugador.Vida > 0 && enemigo.Vida > 0)
+    {
+        
+
+        MostrarEstado(jugador, enemigo);
+        int opcion = ObtenerOpcion();
+        if (opcion == 1)
+        {
+            jugador.Atacar(enemigo);
+        }
+        else if (opcion == 2)
+        {
+            int proba = new Random().Next(0, 100);
+            if (proba < 50)
+            {
+                Console.WriteLine("La fusión ha fallado. Pierdes vida.");
+                jugador.RecibirDanio((int)(jugador.Vida * 0.1f));
+                enemigo.Atacar(jugador);
+                continue;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                jugador = jugador + enemigo;
+                Console.WriteLine($"Has creado un nuevo guerrero: {jugador.Nombre}");
+            }
+        }
+        else
+        {
+            throw new ArgumentException("Opcion invalida. Debe ser 1 o 2.");
+        }
+
+        //enemigo ataca si sigue vivo
+        if (enemigo.Vida > 0)
+        {
+            enemigo.Atacar(jugador);
+        }
+        else if (jugador.Vida > 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Has derrotado a {enemigo.Nombre}!");
+        }
+        else {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("perdiste");
+        }
+          /*  Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(jugador.Vida>0 ? "Haz ganado!!" : "Perdiste..."); */
+    }
+
 
 }
-catch
-{
 
+catch (Exception ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Ha ocurrido un error inesperado: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Gracias por jugar!");
 }
 
 static string ObtenerNombre()
@@ -100,5 +164,12 @@ static Guerrero GenerarEnemigo()
     int ataque = ataques[new Random().Next(ataques.Length)];
 
     return new Guerrero(nombre, vida, ataque);
-
+}
+//Funcion mostrar el esatdo
+static void MostrarEstado(Guerrero jugador, Guerrero enemigo)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"Tu vida : {jugador.Vida} | Vida del enemigo: {enemigo.Vida}");
+    Console.WriteLine("1 Atacar");
+    Console.WriteLine("2 Fusionar");
 }
